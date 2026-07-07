d59deebba86a57cb0035da0154a5408bb214ab8b
SofieRazou <sofia.razoy@gmail.com>
2026-07-07 16:51:13 +0200

    Update shared_mem_manager.py

--- Work/code/shared_mem_manager.py
+++ Work/code/shared_mem_manager.py
@@ -2,10 +2,10 @@ import sys
 import numpy as np
 from multiprocessing import shared_memory
 
-BUFFER_SIZE = 1024
-sine_array = np.sin(np.linspace(0, 2 * np.pi, BUFFER_SIZE)).astype(np.float64)
+BUFFER_SIZE = 8
+sine_array=np.sin(np.linspace(0, 2 * np.pi, BUFFER_SIZE)).astype(np.float32)
 class SManager:
-    def create_mem (self, mem_name="shared_mem", size=1024):
+    def create_mem (self, mem_name="shared_mem", size=BUFFER_SIZE):
         sm = shared_memory.SharedMemory(create=True, size= BUFFER_SIZE, name = "udp_share")
         b = np.ndarray(sine_array.shape, dtype=sine_array.dtype, buffer=sm.buf)
         b[:] = sine_array[:]
@@ -16,3 +16,12 @@ class SManager:
         pass
     def upd(self, data, write_enable):
         pass
+
+def main():
+    sm, mem_data = SManager().create_mem(mem_name="udp_share", size=BUFFER_SIZE)
+    sine_array = np.sin(np.linspace(0, 2 * np.pi, BUFFER_SIZE)).astype(np.float32)
+    mem_data[:] = sine_array[:]
+    
+
+if __name__ == "__main__":
+    main()
