398b7361842a0924151784f6db4fc86799cef7f7
SofieRazou <sofia.razoy@gmail.com>
2026-07-16 14:28:40 +0200

    Update gui_trigger.py

--- Work/code/gui_trigger.py
+++ Work/code/gui_trigger.py
@@ -2,18 +2,26 @@ import sys
 import time
 import subprocess 
 import threading
-pid_2 = "gui_arch.py"
+import matlab.engine
+
 pid_1 = "udp.py"
+pid_2 = "spring_sim.py"
+pid_3 = "C:\Users\javot\Documents\MATLAB\Bern\simulink\sofia_udp_test.m"
+
+eng = matlab.engine.start_matlab()
+eng.sofia_udp_test(nargout=0)
 
 process1 = subprocess.Popen([sys.executable, pid_1])
 time.sleep(2)
 process2 = subprocess.Popen([sys.executable, pid_2])
+
 
 try:
     process2.wait()
 finally:
     process1.terminate()
     process1.wait()
+    eng.quit()
 
 
 print("GUI execution terminated...")
