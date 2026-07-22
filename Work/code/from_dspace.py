1f9fb405cf84436016283eb23ad4eb97bdfeddae
SofieRazou <sofia.razoy@gmail.com>
2026-07-22 15:38:55 +0200

    Update from_dspace.py

--- Work/code/from_dspace.py
+++ Work/code/from_dspace.py
@@ -1,36 +1,76 @@
+# import socket
+# import time
+
+# UDP_IP = "127.0.0.1"      # Same PC
+# UDP_PORT = 50000
+
+# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# counter = 0
+
+# while counter<10:
+#     try:
+#         message = f"HELLO FROM DSPACE {counter}"
+
+#         sock.sendto(message.encode("utf-8"), (UDP_IP, UDP_PORT))
+
+#         print("Sent:", message)
+
+#         counter += 1
+#         for i in range(len(dir(plat.ActiveVariableDescription.Variables))):
+#                 var = plat.ActiveVariableDescription.Variables.Item(i)
+#                 vars.append({var.Name, var.ValueConverted})
+#                 if ((var.Name == 'AO_ch8') or (var.Name== 'AO_ch16') or (var.Name=='Torque')):
+#                     my_vars.append({var.Name, var.ValueConverted})   
+#                     print(f"Value of {var.Name} is {var.ValueConverted}")
+#         time.sleep(1)
+
+#     except KeyboardInterrupt:
+#         print("Exciting with Ctrl+C...")
+
+
+
+# vars = []
+# my_vars = []
+
+
 import socket
 import time
+import json
 
-UDP_IP = "127.0.0.1"      # Same PC
+UDP_IP = "127.0.0.1"
 UDP_PORT = 50000
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-counter = 0
+interesting = ["AO_ch8", "AO_ch16", "Torque"]
 
-while counter<10:
-    try:
-        message = f"HELLO FROM DSPACE {counter}"
+try:
+    while True:
 
-        sock.sendto(message.encode("utf-8"), (UDP_IP, UDP_PORT))
+        values = {}
 
-        print("Sent:", message)
+        # Iterate through all dSPACE variables
+        for i in range(plat.ActiveVariableDescription.Variables.Count):
 
-        counter += 1
-        for i in range(len(dir(plat.ActiveVariableDescription.Variables))):
-                var = plat.ActiveVariableDescription.Variables.Item(i)
-                vars.append({var.Name, var.ValueConverted})
-                if ((var.Name == 'AO_ch8') or (var.Name== 'AO_ch16') or (var.Name=='Torque')):
-                    my_vars.append({var.Name, var.ValueConverted})   
-                    print(f"Value of {var.Name} is {var.ValueConverted}")
-        time.sleep(1)
+            var = plat.ActiveVariableDescription.Variables.Item(i)
 
-    except KeyboardInterrupt:
-        print("Exciting with Ctrl+C...")
+            if var.Name in interesting:
+                values[var.Name] = var.ValueConverted
 
+        # Convert to JSON
+        packet = json.dumps(values)
 
+        # Send
+        sock.sendto(packet.encode("utf-8"), (UDP_IP, UDP_PORT))
 
-vars = []
-my_vars = []
-       
+        print(packet)
+
+        time.sleep(0.01)      # 100 Hz
+
+except KeyboardInterrupt:
+    print("Stopped.")
+
+finally:
+    sock.close()
 # all dSpace model variables ['finalTime', 'currentTime', 'modelStepSize', 'simState', 'errorNumber', 'sumOfResetTime', 'Active ErrorSet', 'Error Activated', 'Error Switching', 'Flags', 'Trigger', 'AO_ch8', 'AO_ch16', 'Torque', 'Out1', 'Out1', 'Out1', 'Out1', 'Out1', 'Out1', 'Out1', 'Out1', 'Out1', 'Out1', 'Out1', 'UpperLimit', 'LowerLimit', 'Out1', 'Out1', 'Out1', 'Out1', 'Out1', 'Out1', 'Out1', 'Gain', 'Out1', 'Gain', 'Out1', 'Gain', 'Out1', 'Gain', 'Out1', 'Gain', 'Value', 'Value', 'Value', 'Value', 'Value', 'Value', 'Value', 'Value', 'Value', 'Value', 'AnalogInput_ch1', 'AnalogInput_ch2', 'ADC', 'ADC', 'Out1', 'Gain', 'Out1', 'Gain', 'Position', 'Speed', 'Index Detected']
