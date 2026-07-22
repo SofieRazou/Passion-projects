ee0295de700baf18729bfaa7b28d942a3717bf5c
SofieRazou <sofia.razoy@gmail.com>
2026-07-22 10:03:38 +0200

    Fix COM error handling in to_dspace.py

--- Work/code/to_dspace.py
+++ Work/code/to_dspace.py
@@ -1,3 +1,28 @@
+
+(capt) C:\Users\javot\Desktop\sofia_code>python from_dspace.py
+Traceback (most recent call last):
+  File "C:\Users\javot\Desktop\capt\lib\site-packages\win32com\client\dynamic.py", line 81, in _GetGoodDispatch
+    IDispatch = pythoncom.connect(IDispatch)
+pywintypes.com_error: (-2147221005, 'Invalid class string', None, None)
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "C:\Users\javot\Desktop\sofia_code\from_dspace.py", line 5, in <module>
+    cd = win32com.client.Dispatch("ControlDesk.Application")
+  File "C:\Users\javot\Desktop\capt\lib\site-packages\win32com\client\__init__.py", line 116, in Dispatch
+    dispatch, userName = dynamic._GetGoodDispatchAndUserName(dispatch, userName, clsctx)
+  File "C:\Users\javot\Desktop\capt\lib\site-packages\win32com\client\dynamic.py", line 101, in _GetGoodDispatchAndUserName
+    return (_GetGoodDispatch(IDispatch, clsctx), userName)
+  File "C:\Users\javot\Desktop\capt\lib\site-packages\win32com\client\dynamic.py", line 83, in _GetGoodDispatch
+    IDispatch = pythoncom.CoCreateInstance(
+pywintypes.com_error: (-2147221005, 'Invalid class string', None, None)
+
+(capt) C:\Users\javot\Desktop\sofia_code>
+
+
+
+
 import win32com.client
 import time
 
