daa8c78b540f96dee6d99c755d054097930c1966
SofieRazou <sofia.razoy@gmail.com>
2026-07-27 10:47:42 +0200

    Update to_sim.py

--- Work/code/to_sim.py
+++ Work/code/to_sim.py
@@ -1,3 +1,10 @@
+Cannot propagate bus signal from 'Output Port 1' of 'driving_scene1/Scenario Reader' to 'Input Port 1' of 'driving_scene1/Demux' because this input port requires a non-bus signal.
+
+If the destination block is a bus-capable block, ensure that the block configuration and its input signal(s) meet the requirements for bus support. Please see Simulink documentation for further information on composite (i.e. bus) signals and their proper usage. Alternately, if the input bus signal is virtual; consists only of scalar elements, 1-D elements, or either row or column vectors; and all elements have the same data type, signal type, and sampling mode, consider inserting a Bus to Vector conversion block in the signal path. Otherwise, consider using a Bus Selector block in the signal path.
+
+
+
+
 import numpy as np
 import socket
 
