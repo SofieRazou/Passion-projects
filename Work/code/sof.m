c41d836ff7cd91010214262243e47f9be1071681
SofieRazou <sofia.razoy@gmail.com>
2026-07-28 14:06:06 +0200

    Update sof.m

--- Work/code/sof.m
+++ Work/code/sof.m
@@ -2,14 +2,29 @@ function yaw = read_yaw()
 
 persistent m
 
+% Default output
+yaw = 0;
+
 if isempty(m)
     filename = 'yaw_shared.bin';
 
-    m = memmapfile(filename, ...
-        'Format', 'double', ...
-        'Writable', false);
+    % Check that the file exists
+    if ~isfile(filename)
+        warning('Shared memory file "%s" not found.', filename);
+        return;
+    end
+
+    try
+        m = memmapfile(filename, ...
+            'Format', 'double', ...
+            'Writable', false);
+    catch ME
+        warning('Failed to open memory-mapped file:\n%s', ME.message);
+        return;
+    end
 end
 
+% Read the yaw value
 yaw = m.Data(1);
 
 end
