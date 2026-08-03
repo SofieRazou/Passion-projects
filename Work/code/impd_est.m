ca21e88d8c89329036c5d911f82e02c8cd2d026e
SofieRazou <sofia.razoy@gmail.com>
2026-08-03 10:38:41 +0200

    Update impd_est.m

--- Work/code/impd_est.m
+++ Work/code/impd_est.m
@@ -11,12 +11,23 @@ else
 end 
 end  *)
 
-function impd_est  = fcn(near_zero, n_crossing,torque, angle, fact)
+function impd_est = fcn(near_zero, n_crossing, torque, angle, fact)
 
-%Boolean cut for impedance estimation near zero
+% Small regularization parameter
+epsilon = 1e-4;
+
+% Shift angle around the zero crossing
+angle_shift = angle - n_crossing*pi;
+
+% Regularized estimates
+impd_zero = fact * angle_shift / (angle_shift^2 + epsilon^2);
+impd_torque = torque * angle / (angle^2 + epsilon^2);
+
+% Smooth transition
 if near_zero
-    impd_est = fact/(angle-n_crossing*pi);
+    impd_est = impd_zero;
 else
-    impd_est = torque/angle;
-end 
-end 
+    impd_est = impd_torque;
+end
+
+end
