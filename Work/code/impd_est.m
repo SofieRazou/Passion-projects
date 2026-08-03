e2e482989a7993ab19099e3e277873ece37e38c3
SofieRazou <sofia.razoy@gmail.com>
2026-08-03 10:37:36 +0200

    Update impd_est.m

--- Work/code/impd_est.m
+++ Work/code/impd_est.m
@@ -10,34 +10,13 @@ else
     
 end 
 end  *)
-function [impd_est, impd_reg_est] = fcn(near_zero, n_crossing, torque, angle, fact)
 
-x = angle - n_crossing*pi;
+function impd_est  = fcn(near_zero, n_crossing,torque, angle, fact)
 
-x_min = 1e-3;
-
+%Boolean cut for impedance estimation near zero
 if near_zero
-    
-    if abs(x) > x_min
-        % Cotangent approximation
-        impd_est = fact*(1/x - x/3);
-    else
-        % Limit the impedance close to the singularity
-        impd_est = fact*(1/x_min - x_min/3)*sign(x);
-    end
-    
-    impd_reg_est = 30;
-
+    impd_est = fact/(angle-n_crossing*pi);
 else
-    
-    impd_est = 1.816;
-    
-    if abs(angle) > x_min
-        impd_reg_est = torque/angle;
-    else
-        impd_reg_est = 30;
-    end
-    
-end
-
-end
+    impd_est = torque/angle;
+end 
+end 
