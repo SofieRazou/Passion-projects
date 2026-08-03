5316242abcac785d8f6976f8e304fecfadd463d9
SofieRazou <sofia.razoy@gmail.com>
2026-08-03 10:05:07 +0200

    Create zero_crossings.m

---
+++ Work/code/zero_crossings.m
@@ -0,0 +1,18 @@
+function crossings = fcn(angle)
+
+persistent crossings_prev
+
+if isempty(crossings_prev) 
+    crossings_prev = 0; 
+end
+
+% Default output 
+crossings = crossings_prev;
+
+% Detect zero crossing
+if angle == 0 
+    crossings_prev = crossings_prev + 1;
+    crossings = crossings_prev; 
+end
+
+end
