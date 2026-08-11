9455c487de927341b3cf99346c9fa5b9ee4b926a
SofieRazou <sofia.razoy@gmail.com>
2026-08-11 10:47:16 +0200

    Update capt_fidelity.m

--- Work/code/capt_fidelity.m
+++ Work/code/capt_fidelity.m
@@ -2,11 +2,11 @@
 %        PURE STIFFNESS (k-only) TRANSFER FUNCTION & BODE
 % =============================================================
 J_val = 0.0103;          % Fixed motor inertia
-k_avg = 42.95;           % Your average estimated stiffness
-
+k_avg = 42.95 + 1;           % Your average estimated stiffness
+b_avg = 9.1079;
 % Define Transfer Function with b = 0
 num = [1];
-den = [J_val, 0, k_avg]; % Notice the 0 for the damping coefficient term
+den = [J_val, b_avg, k_avg]; % Notice the 0 for the damping coefficient term
 sys_k_only = tf(num, den);
 
 disp('--- PURE STIFFNESS TRANSFER FUNCTION (b=0) ---');
@@ -16,4 +16,4 @@ disp(sys_k_only);
 figure('Name', 'k-Only System Bode Diagram');
 bode(sys_k_only);
 grid on;
-title(sprintf('Bode Plot (Pure Stiffness: k = %.2f, b = 0)', k_avg));
+title(sprintf('Bode Plot (k = %.2f, b = %.2f)', k_avg, b_avg));
